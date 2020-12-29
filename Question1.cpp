25f85a0d2474e2144a3201a1e02fb2c129629a99
suresh52 <40735229+suresh52@users.noreply.github.com>
2020-12-30 00:09:06 +0530

    Create Question1.cpp
    Palindrome logic in CPP

---
+++ Question1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+bool is_Palindrome(std::string str)
+{
+    int len = str.length();
+    for(int i=0;i<=len/2;i++) 
+    {
+        if(tolower(str[i]) != tolower(str[len-i-1]))
+            {
+                return false;
+            }
+    }
+    return true;
+}
+int main()
+{
+    if(is_Palindrome("Step on no pets"))
+    {
+        std::cout <<"given string is Palindrome" << std::endl;
+    }
+    else
+    {
+        std::cout <<"given string is Not a Palindrome" << std::endl;
+    }
+    
+}
